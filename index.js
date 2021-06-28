9f25eed80a77256cf506fa68239574950ca3cb31
Andrew Masih <armasih@outlook.com>
2021-06-28 18:19:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let desserts = ["Banoffee pie", "Espresso Martini Cheesecake.", " Carrot cake", "Victoria sponge", "Apple crumble"];
+
+
+for (let i = 0; i < desserts.length; i ++); {
+  
+  console.log("i is equal to: 0");
+  console.log("desserts [i]");
+  console.log("desserts");
+  console.log(" ");
+
+};
